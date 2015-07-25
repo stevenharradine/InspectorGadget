f0040a372a801689218548558dfbbd5f116bbbb7
Steven Harradine <stevenharradine@gmail.com>
2015-07-25 16:17:53 -0400

    add domain, add new line to eof

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-module.exports.PORT            = 3000
-module.exports.USERID          = 'ubuntu'
-module.exports.CPU_MULTIPLIER  = 1
+module.exports.PORT           = 3000
+module.exports.USERID         = "ubuntu"
+module.exports.CPU_MULTIPLIER = 1
+module.exports.DOMAIN         = "example.com"
